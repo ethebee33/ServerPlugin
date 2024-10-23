b3ac39dc18fc9202d15b7677b67195704411e733
DaGaminMonke101 <tc011081@gmail.com>
2024-10-23 21:26:54 +0100

    testy2

--- src/main/java/ethebee3/ServerPlugin/discord/discordBot.java
+++ src/main/java/ethebee3/ServerPlugin/discord/discordBot.java
@@ -4,6 +4,7 @@ public class discordBot {
     //nothing here yet, i have to disect discordsrv in order to do this
     //might just fork a discord bot plugin and add it here, its not public anyway so idrc
     //stupid test thing
+    //this is another stupid test thing
 
     public void registerBot() {}
 
